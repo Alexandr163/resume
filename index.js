4ce683a3b5c8b54c567c73cb7129f9d089e99c73
Alexandr Morgunkov <shadow091@mail.ru>
2022-06-21 23:06:00 +0400

    change new paragraph in the .info-tex

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const getElement = document.querySelector(".info-text");
 console.log(getElement);
 const newParagraph = document.createElement("p");
-newParagraph.textContent = "Прохожу курс Frontend Developer в школе Result School, поставил перед собой цель работать в IT";
+newParagraph.textContent = "Прохожу курс Junior Frontend Developer в школе Result School, поставил перед собой цель работать в IT";
 getElement.append(newParagraph);
